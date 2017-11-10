7b212769d628d149389482be05551004f6b03a06
harindaka <harindaka@gmail.com>
2017-11-10 12:10:02 +0530

    changed the build generated file name to file_path.txt

--- build.rs
+++ build.rs
@@ -1,11 +1,11 @@
-use std::{iter, env};
+use std::{env};
 use std::path::Path;
 use std::io::{Write, BufWriter};
 use std::fs::File;
 
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
-    let dest_path = Path::new(&out_dir).join("long_string.txt");
+    let dest_path = Path::new(&out_dir).join("file_path.txt");
     let mut f = BufWriter::new(File::create(&dest_path).unwrap());
 
     let long_string = dest_path.display();
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
     
-    static LONG_STRING: &'static str = include_str!("long_string.txt");
+    static LONG_STRING: &'static str = include_str!("file_path.txt");
     println!("{}", LONG_STRING);
 }
