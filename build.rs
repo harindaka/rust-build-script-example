434c28157e136d91df110657b8dc756651c62966
harindaka <harindaka@gmail.com>
2017-11-10 16:03:56 +0530

    used build_script_file_gen crate's include_file_str! macro

--- build.rs
+++ build.rs
@@ -14,8 +14,8 @@ fn main() {
     let hello_world = "Hello World!";
     write!(f, "{}", hello_world).unwrap();
 
-    //The same above can be achieved using the build_script_file_gen crate as follows
-    let file_name = String::from("gen_file.txt");
-    let content = String::from("This file content was added using the build_script_file_gen crate's gen_file function");
-    gen_file(&file_name, &content);
+    //The same functionality above can be achieved using 
+    //the build_script_file_gen crate's gen_file method
+    let file_content = "This content was added and included using the build_script_file_gen crate's gen_file function and include_file_str! macro respectively";
+    gen_file("gen_file.txt", &file_content);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
+#[macro_use]
+extern crate build_script_file_gen;
+
 fn main() {
     
-    static STRING_ONE: &'static str = include_str!(concat!(env!("OUT_DIR"), "/hello_world.txt"));
-    static STRING_TWO: &'static str = include_str!(concat!(env!("OUT_DIR"), "/gen_file.txt"));
-
+    static STRING_ONE: &'static str = include_str!(concat!(env!("OUT_DIR"), "/hello_world.txt"));    
     println!("{}", STRING_ONE);
-    println!("{}", STRING_TWO);
+    
+    //The same functionality above can be achieved using 
+    //the build_script_file_gen crate's include_file_str! macro
+    println!(include_file_str!("/gen_file.txt"));
 }
