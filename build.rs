d9dfb236872a24005651bd0f82ecde1b9e202709
harindaka <harindaka@gmail.com>
2017-11-10 13:06:28 +0530

    changed adding path in file to adding hellow world text

--- build.rs
+++ build.rs
@@ -5,9 +5,9 @@ use std::fs::File;
 
 fn main() {
     let out_dir = env::var("OUT_DIR").unwrap();
-    let dest_path = Path::new(&out_dir).join("file_path.txt");
+    let dest_path = Path::new(&out_dir).join("hello_world.txt");
     let mut f = BufWriter::new(File::create(&dest_path).unwrap());
 
-    let long_string = dest_path.display();
-    write!(f, "{}", long_string).unwrap();
+    let hello_world = "Hello World!";
+    write!(f, "{}", hello_world).unwrap();
 }
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     
-    static LONG_STRING: &'static str = include_str!(concat!(env!("OUT_DIR"), "/file_path.txt"));
+    static LONG_STRING: &'static str = include_str!(concat!(env!("OUT_DIR"), "/hello_world.txt"));
 
     println!("{}", LONG_STRING);
 }
